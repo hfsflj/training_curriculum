343ced2e388291779445ff52072fc5e07c4a32bf
hfsflj <147462589+hfsflj@users.noreply.github.com>
2023-10-17 17:28:14 +0900

    Issue4~6の解消
    Issue4~6の解消

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,4 @@
 class CalendarsController < ApplicationController
-
   # １週間のカレンダーと予定が表示されるページ
   def index
     get_week
@@ -13,17 +12,16 @@ class CalendarsController < ApplicationController
   end
 
   private
+
   def plan_params
     params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-  end
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
 
@@ -35,14 +33,15 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = @todays_date + x
-      if #「wday_numが7以上の場合」という条件式
-        wday_num = wday_num -7
-      end
+      wday_num = (@todays_date.wday + x) % 7
 
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: [(@todays_date+x).wday] }
+      days = { 
+        month: (@todays_date + x).month, 
+        date: (@todays_date + x).day, 
+        plans: today_plans, 
+        wday: wdays[wday_num]  # 曜日を表示
+      }
       @week_days.push(days)
     end
+  end
 end
-
-
